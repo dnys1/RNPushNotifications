a325e85b270d0ad3fe2c66f023c37423bd957ace
Dillon Nys <nydillon@amazon.com>
2021-08-31 12:09:54 -0700

    Add push notifications

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Amplify from 'aws-amplify';
-import config from './src/aws-exports';
-Amplify.configure(config);
+import PushNotification from '@aws-amplify/pushnotification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);
